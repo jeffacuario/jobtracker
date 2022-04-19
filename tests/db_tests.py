16872d8a7d66d12e06d578c454a084d58beb66b2
thesteau <25945183+thesteau@users.noreply.github.com>
2022-04-19 13:12:42 -0700

    Commented out unused code

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -1,9 +1,9 @@
 import unittest
-from unittest.mock import Mock
+# from unittest.mock import Mock
 
 import firebase_admin
 from firebase_admin import firestore
-from website.models import db as db_funcs
+# from website.models import db as db_funcs
 
 
 class MyTestCase(unittest.TestCase):
