94ed3b4f8cd50bec64997fd9a16b90e249af6fe4
thesteau <25945183+thesteau@users.noreply.github.com>
2022-04-19 13:23:12 -0700

    Add noqa to import statement

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -2,6 +2,7 @@ import unittest
 # from unittest.mock import Mock
 
 import firebase_admin
+from firebase_admin import firestore    # noqs: F401
 # from website.models import db as db_funcs
 
 
@@ -23,7 +24,6 @@ class MyTestCase(unittest.TestCase):
                 Else, this will perform "mock" tests to a "mock" database.
         """
         try:
-            from firebase_admin import firestore
             with open("../private/json_file_name.txt", "r") as db_file:
                 file_json = db_file.read()
 
