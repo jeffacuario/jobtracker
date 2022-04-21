5fc87bedb39e765745008e57a9db8edc0ca6970b
Jeffrey Acuario <62631558+jeffacuario@users.noreply.github.com>
2022-04-21 08:03:23 -0700

    Update db.py

--- website/models/db.py
+++ website/models/db.py
@@ -4,6 +4,6 @@ import firebase_admin as fa
 def db_init():
     """ Log in creds"""
     # Firebase credentials
-    file_json = "jobtrack-39d73-firebase-adminsdk-wpmtz-1bb461d761.json"
+    file_json = "credentials.json”"
     cred = fa.credentials.Certificate("./private/" + file_json)
     fa.initialize_app(cred)
