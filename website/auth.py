f3c3fdb94022f42c7c06d1f023a10c593d5b650a
Paul Min <isimplyexcel@gmail.com>
2022-04-22 22:17:23 -0700

    implement user login functionality with pyrebase and flask sessions

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from distutils.command.config import config
-from flask import Blueprint, render_template, request, redirect, url_for
-import json, pyrebase, requests
+from flask import Blueprint, render_template, request, redirect, url_for, g, session
+from firebase_admin import auth as fa_auth
+import json, pyrebase, requests, functools
 
 
 auth = Blueprint('auth', __name__)
@@ -14,11 +15,31 @@ firebase = pyrebase.initialize_app(config)
 fb_auth = firebase.auth()
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
     """
     Render Login Page
     """
+    if request.method == 'POST':
+        username = request.form['inputEmail']
+        password = request.form['inputPassword']
+
+        error_message = None
+
+        try:
+            user = fb_auth.sign_in_with_email_and_password(username, password)
+            print(user)
+            session.clear()
+            session['user_id'] = user['localId']
+            session['token_id'] = user['idToken']
+            return redirect(url_for('views.jobs'))
+        except requests.exceptions.HTTPError as e:
+            error_json = e.args[1]
+            error = json.loads(error_json)['error']['message']
+            print(error)
+            if error == 'INVALID PASSWORD' or error == 'EMAIL_NOT_FOUND':
+                error_message = "You have entered an invalid email or password"
+            return render_template("auth/login.html", error_message=error_message)
     return render_template("auth/login.html")
 
 
@@ -28,15 +49,17 @@ def register():
     Render Register Page
     """
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['inputName']
+        email = request.form['inputEmail']
+        password = request.form['inputPassword']
 
-        error = None
         error_message = None
 
         try:
-            fb_auth.create_user_with_email_and_password(username, password)
+            user = fb_auth.create_user_with_email_and_password(email, password)
+            fa_auth.update_user(user.get("localId"), display_name=username)
         except requests.exceptions.HTTPError as e: # we will need to handle the exception and display error to user
+            print(e)
             error_json = e.args[1]
             error = json.loads(error_json)['error']['message']
             print(error)
@@ -49,4 +72,39 @@ def register():
             return redirect(url_for("auth.login"))
 
     return render_template('auth/register.html')
+
+
+@auth.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    token_id = session.get('token_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        try:
+            g.user = fb_auth.get_account_info(token_id)
+            print(g.user)
+        except requests.exceptions.HTTPError as e:
+            error_json = e.args[1]
+            error = json.loads(error_json)['error']['message']
+            print(error)
+            if error == 'INVALID_ID_TOKEN':
+                return redirect(url_for("auth.logout"))
+
+@auth.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('views.jobs'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
 
